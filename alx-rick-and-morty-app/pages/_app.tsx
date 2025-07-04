e9e17c6b6dd0c939b0f36561a1f8d4fba2681d75
Rediet Abreham <nara1415karol@gmail.com>
2025-07-04 22:36:55 +0300

    Create a Component to Test ErrorBoundary

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -1,14 +1,14 @@
 import ErrorBoundary from '@/components/ErrorBoundary';
-import type { AppProps } from "next/app";
+import ErrorProneComponent from '@/components/ErrorProneComponent';
 
 
 
-function MyApp({ Component, pageProps }: AppProps) {
+const Home: React.FC = () => {
   return (
     <ErrorBoundary>
-      <Component {...pageProps} />
+      <ErrorProneComponent />
     </ErrorBoundary>
   );
-}
+};
 
-export default MyApp;
+export default Home;
