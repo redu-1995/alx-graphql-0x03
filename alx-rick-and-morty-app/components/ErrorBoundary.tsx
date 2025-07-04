7158d9cfe41ff11b6ed06e2f6ca8d687e8645557
Rediet Abreham <nara1415karol@gmail.com>
2025-07-04 22:33:45 +0300

    Wrap the Application with ErrorBoundary

--- alx-rick-and-morty-app/components/ErrorBoundary.tsx
+++ alx-rick-and-morty-app/components/ErrorBoundary.tsx
@@ -1,3 +1,5 @@
+import React, { ReactNode } from 'react';
+
 interface State {
   hasError: boolean;
 }
@@ -6,8 +8,7 @@ interface ErrorBoundaryProps {
   children: ReactNode;
 }
 
-
-class ErrorBoundary extends React.Component<ErrorBoundaryProps , State> {
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, State> {
   constructor(props: ErrorBoundaryProps) {
     super(props);
     this.state = { hasError: false };
